37238891deb742090aa337d52c5054530eb3823b
kepkakarol <kepkakarol@gmail.com>
2016-02-08 23:10:40 +0100

    test.py

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
-print("hello")
+#!/usr/bin/env python
 
 print("hello")
+
+
+def repeat(s, exclaim):
+    result = s + s + s;
+    if exclaim:
+        result += '!!!'
+
+    return result
+
+def test() :
+    liczba = input("Podaj liczbę: ")
+    print(liczba)
+print(repeat("aa", True))
+test()
